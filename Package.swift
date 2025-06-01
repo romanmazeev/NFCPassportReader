d6a80cfd86a379b7f08c8ff61a41258109bdc63a
Roman Mazeev <roman.mazeev@lightspeedhq.com>
2025-06-01 16:28:15 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "NFCPassportReader",
-    platforms: [.iOS("15.0")],
+    platforms: [.iOS(.v14), .macOS(.v11)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
